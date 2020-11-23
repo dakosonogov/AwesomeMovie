7455e060be3af86f4b72caf126dfdd8ec950fbfd
submarkos <dakosonogov@gmail.com>
2020-11-23 13:30:42 +0300

    Homework #View & ViewGroups

---
+++ app/src/main/java/ru/kosonogov/awesomemovie/MovieDetailsActivity.kt
@@ -0,0 +1,11 @@
+package ru.kosonogov.awesomemovie
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MovieDetailsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_movie_details)
+    }
+}
