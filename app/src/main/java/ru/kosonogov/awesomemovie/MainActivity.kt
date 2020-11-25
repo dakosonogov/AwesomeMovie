f3811bb98cf051a7df244ce643b184981358619b
submarkos <dakosonogov@gmail.com>
2020-11-25 15:46:04 +0300

    Mentor's feedback fixes

--- app/src/main/java/ru/kosonogov/awesomemovie/MainActivity.kt
+++ app/src/main/java/ru/kosonogov/awesomemovie/MainActivity.kt
@@ -4,15 +4,16 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import ru.kosonogov.awesomemovie.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val button: Button = findViewById(R.id.button)
-
-        button.setOnClickListener {
+        binding.button.setOnClickListener {
             val intent = Intent(this, MovieDetailsActivity::class.java)
             startActivity(intent)
         }
